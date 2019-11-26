d60001d58dabd0e946f013ebc190918064439cbe
zhaofeng <616545598@qq.com>
2019-11-26 16:55:18 +0800

    Upload New File

---
+++ download-curl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# curl --head url Content-Length:  13923888724
+set -e -x
+url=https://cvml.ist.ac.at/AwA2/AwA2-base.zip
+total_bytes=$(curl --head $url | grep Content-Length | awk '{split($0,a," ");print a[2]}' | awk '{print substr($0, 1, length($0)-1)}') 
+start_id=$SLURM_ARRAY_TASK_ID
+end_id=$((SLURM_ARRAY_TASK_ID+1))
+total_id=$((SLURM_ARRAY_TASK_MAX+1))
+echo $total_bytes
+echo $total_id
+start=$((start_id * total_bytes / total_id))
+end=$((end_id * total_bytes / total_id))
+#echo $filename
+curl -H "Range: bytes=$start-$end" $url -o $start-$end.partial.zip
