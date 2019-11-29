40c4b3c2523945cf36b8ef71e58e7338adbd0dd9
zhaofeng <616545598@qq.com>
2019-11-29 16:03:21 +0800

    Update cpu_limit.sh

--- maintenance/cpu_limit.sh
+++ maintenance/cpu_limit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # get the top 5 processes
 result=1490.0
-mapfile -t proc_list < <(ps --no-headers --sort=-pcpu -Ao pid,pcpu | head -n 5)
+mapfile -t proc_list < <(ps --no-headers --sort=-pcpu -Ao pid,pcpu,user,cmd | head -n 5)
 for i in "${proc_list[@]}"; do
     process_cpu_list=($i)
     process_id=${process_cpu_list[0]}
@@ -9,5 +9,8 @@ for i in "${proc_list[@]}"; do
     compare_result=$(awk 'BEGIN{ print '$cpu_usage_percentage'<'$result' }')
     if [ ! "$compare_result" -eq 1 ]; then
       kill $process_id
+      echo $(date)
+      echo ${process_cpu_list[2]}
+      echo ${process_cpu_list[3]}
     fi
 done
