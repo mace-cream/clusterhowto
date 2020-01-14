4be501fe8bc4c9b0d4730c64535a4d5744c14407
zhaofeng <616545598@qq.com>
2020-01-14 16:49:34 +0800

    Update download-curl.sh

--- download-curl.sh
+++ download-curl.sh
@@ -6,6 +6,7 @@
 # Current limitation: the file prefix is hard coded, e.g. zip format.
 set -e -x
 url=$1
+suffix="${url##*.}"
 total_bytes=$(curl --head $url | grep Content-Length | awk '{split($0,a," ");print a[2]}' | awk '{print substr($0, 1, length($0)-1)}') 
 start_id=$SLURM_ARRAY_TASK_ID
 end_id=$((SLURM_ARRAY_TASK_ID+1))
@@ -14,6 +15,6 @@ echo $total_bytes
 echo $total_id
 start=$((start_id * total_bytes / total_id))
 end=$((end_id * total_bytes / total_id -1))
-filename=$(printf %02d $start_id).partial.zip
+filename=$(printf %02d $start_id).partial.$suffix
 curl -H "Range: bytes=$start-$end" $url -o $filename
-# after the downloading complete, please use `cat *.partial.zip > total.zip` to join the partial files together
+# after the downloading complete, please use `cat *.partial.$suffix > total.$suffix` to join the partial files together
