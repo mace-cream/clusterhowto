6f5eb86bba4c62edf90f40192a13e11b3a6fba2a
An Zhicheng <32831287+anzhicheng@users.noreply.github.com>
2020-04-10 00:32:55 +0800

    Update verify_password.py

--- maintenance/verify_password.py
+++ maintenance/verify_password.py
@@ -1,18 +1,18 @@
 # this script is used to check whether username and password are
 # the same for given ip and port
 
-# How to use this script on our server
-# module load anaconda2/py2
-# pip install --user ssh2-python
-# python verify_password.py
 import socket
 from ssh2.session import Session
 from ssh2.exceptions import AuthenticationError
 
 ip = '10.8.4.170'
 port = 22
-userlist = ['lewenyuan','lianjing','riccardo','tanyang','tianzhou','ziyanzheng']
-
+# userlist = ['lewenyuan','lianjing','riccardo','tanyang','tianzhou','ziyanzheng']
+# userlist = ['wangbin','lirong','jielian','liangcheng','tianyu','hutianyu','jiarongli','anzhicheng','bunchalit','jackkuo','jinggewang',
+#             'luis','jihongwang','lewenyuan','tanyang','ziyanzheng','yurunpeng']
+userlist = []
+for line in open('recore.txt','r'):
+    userlist.append(line.replace('\n',''))
 def ssh_login(user):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip,port))
@@ -25,6 +25,39 @@ def ssh_login(user):
         return
     print(user)
 
+def ssh_login2(user):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip,port))
+    sock.setblocking(1)
+    session = Session()
+    session.handshake(sock)
+    try:
+        session.userauth_password(user,'password')
+    except AuthenticationError:
+        return
+    print(user)
+
+def ssh_login3(user):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip,port))
+    sock.setblocking(1)
+    session = Session()
+    session.handshake(sock)
+    try:
+        session.userauth_password(user,'123456')
+    except AuthenticationError:
+        return
+    print(user)
+
 if __name__ == '__main__':
+    print('username------')
     for user in userlist:
         ssh_login(user)
+    print('password-------')
+    for user in userlist:
+        ssh_login2(user)
+    print('123456------')
+    for user in userlist:
+        ssh_login3(user)
+    
+
